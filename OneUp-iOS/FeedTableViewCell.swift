30e559abbe137c70f875829884c2b2fac639666a
mkausas <mkausas@gmail.com>
2016-02-25 11:13:44 -0500

    Implemented tableviewcell design into FeedTableViewCell.xib

---
+++ OneUp-iOS/FeedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedTableViewCell.swift
+//  OneUp-iOS
+//
+//  Created by Martynas Kausas on 2/25/16.
+//  Copyright © 2016 Kiwi. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
