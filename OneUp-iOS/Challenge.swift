a60335b0e20421808df46d7c460ebe379c93f9f2
Harris Christiansen (hc15ret2) <harrischristiansen@mac.com>
2016-03-03 22:14:28 -0500

    Conformed Challenge Object to NSObject and MKAnnotation

--- OneUp-iOS/Challenge.swift
+++ OneUp-iOS/Challenge.swift
@@ -7,22 +7,25 @@
 //
 
 import Foundation
+import MapKit
 
-class Challenge {
+class Challenge: NSObject, MKAnnotation {
     
     var name: String!
     var attempts: NSArray!
-    var description: String?
+    var desc: String?
     var categories: [String]
     var pattern: String!
     var imgUrl: String!
+    let coordinate: CLLocationCoordinate2D
     
     init(challengeDetails: NSDictionary) {
         name = challengeDetails["name"] as! String
         attempts = challengeDetails["attempts"] as! NSArray
-        description = challengeDetails["description"] as? String ?? "No Description"
+        desc = challengeDetails["description"] as? String ?? "No Description"
         categories = challengeDetails["categories"] as! [String]
         pattern = challengeDetails["pattern"] as! String
+        coordinate = CLLocationCoordinate2D(latitude: 21.282778, longitude: -157.829444)
 //        imgUrl = challengeDetails["imgUrl"] as! String
 
         
