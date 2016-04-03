162db6fb047ad2e35083b81cedfa1186dcf5b662
mkausas <mkausas@gmail.com>
2016-04-03 13:06:20 -0400

    Added basic challenge creation view and controller with ability to grab media content (images and videos) from your phone

---
+++ OneUp-iOS/ChallengeCreationViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ChallengeCreationViewController.swift
+//  OneUp-iOS
+//
+//  Created by Martynas Kausas on 4/3/16.
+//  Copyright © 2016 Kiwi. All rights reserved.
+//
+
+import UIKit
+
+class ChallengeCreationViewController: UIViewController {
+
+    @IBOutlet weak var challengeImageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onSelectMedia(sender: AnyObject) {
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+//        vc.sourceType = UIImagePickerControllerSourceType.Camera // use for pics from camera
+        vc.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        
+        
+        self.presentViewController(vc, animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension ChallengeCreationViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        // Get the image captured by the UIImagePickerController
+//        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        
+        // Do something with the images (based on your use case)
+        challengeImageView.image = editedImage
+        
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+}
+
+extension ChallengeCreationViewController: UINavigationControllerDelegate {
+    
+}
