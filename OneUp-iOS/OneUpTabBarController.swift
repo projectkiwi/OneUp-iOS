ed226bc20e9aa55e6d25911152ed53af55041076
Harris Christiansen (hc15ret2) <harrischristiansen@mac.com>
2016-04-12 04:22:53 -0400

    Added tab bar controller

---
+++ OneUp-iOS/OneUpTabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarController.swift
+//  OneUp-iOS
+//
+//  Created by Harris Christiansen on 4/12/16.
+//  Copyright © 2016 Kiwi. All rights reserved.
+//
+
+import UIKit
+
+class OneUpTabBarController: UITabBarController {
+    
+    @IBOutlet weak var OneUpTabBar: UITabBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //OneUpTabBar.items?[0].image = UIImage()
+    }
+}
